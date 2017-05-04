b16df05dff091e87a448007c8ea85d09a0a79420
Leinnan <rybopiotr@gmail.com>
2017-05-04 08:55:49 +0200

    source option

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QtCore>
 #include <iostream>
+#include <string>
 #include "account.h"
 #include "transaction.h"
 
@@ -13,17 +14,26 @@ int main(int argc, char *argv[]){
     parser.addHelpOption();
     parser.addVersionOption();
 
+    QCommandLineOption sourceOption(QStringList() << "s" << "source",
+            QCoreApplication::translate("main", "Read from <file>."),
+            QCoreApplication::translate("main", "file"));
+    parser.addOption(sourceOption);
+
+
     parser.process(app);
 
 
-
+    std::string json_path = "test.json";
     la::Account test_account;
     la::Transaction test("22.11.2014 11:14",345345,"Nowa");
 
-    test_account.readFromJson("test.json");
+    if(parser.isSet(sourceOption))
+        json_path = parser.value(sourceOption).toStdString();
 
-    test_account.addTransaction(test);
-    test_account.saveToJson("test_2.json");
+
+    test_account.readFromJson(json_path);
+    test_account.sortTransactions();
+    test_account.saveToJson(json_path);
     test_account.showTransactions();
     test_account.showAccountBalance();
     return 0;
