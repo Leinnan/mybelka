1842659c1afa2447ff57eacca379f45061317d47
Leinnan <rybopiotr@gmail.com>
2017-05-04 08:45:46 +0200

    Start adding argument parser

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 #include "transaction.h"
 
 int main(int argc, char *argv[]){
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("my belka");
+    QCoreApplication::setApplicationVersion("0.1");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("My simple accounting system, still very early version, written in C++ with QTCore");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.process(app);
+
+
+
     la::Account test_account;
     la::Transaction test("22.11.2014 11:14",345345,"Nowa");
 
