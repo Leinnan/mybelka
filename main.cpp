80791c66fa66e5b6a893efda230c20dc4feda7c9
Leinnan <rybopiotr@gmail.com>
2017-05-04 09:40:39 +0200

    Adding new transactions from CMD

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 #include "account.h"
 #include "transaction.h"
 
+namespace cmd{
+
+    la::Transaction addTransaction(){
+        std::string m_name;
+
+        int m_amount = 0;
+
+        std::cout << "\e[1mAdding new transaction\e[0m\n"
+                  << "\nPlease input name of transaction: ";
+        std::cin >> m_name;
+
+        std::cout << "Now lets type amount: ";
+        std::cin >> m_amount;
+        m_amount *= 100;
+
+        return la::Transaction(m_amount,m_name);
+    }
+}
+
 int main(int argc, char *argv[]){
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("my belka");
@@ -19,19 +38,28 @@ int main(int argc, char *argv[]){
             QCoreApplication::translate("main", "file"));
     parser.addOption(sourceOption);
 
+    QCommandLineOption addOption(QStringList() << "a" << "add-new",
+            QCoreApplication::translate("main", "Add new transaction."));
+    parser.addOption(addOption);
+
 
     parser.process(app);
 
 
     std::string json_path = "test.json";
     la::Account test_account;
-    la::Transaction test("22.11.2014 11:14",345345,"Nowa");
 
     if(parser.isSet(sourceOption))
         json_path = parser.value(sourceOption).toStdString();
 
 
     test_account.readFromJson(json_path);
+
+    if(parser.isSet(addOption)){
+        test_account.addTransaction(cmd::addTransaction());
+    }
+
+
     test_account.sortTransactions();
     test_account.saveToJson(json_path);
     test_account.showTransactions();
